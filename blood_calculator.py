fde5fe518d7cc0635087f8e74bac62689e462119
Rachel Welscott <rachel.welscott@gmail.com>
2021-08-30 12:43:38 -0400

    set up basic interface

---
+++ blood_calculator.py
@@ -0,0 +1,9 @@
+def interface():
+    print("Blood Calculator")
+    print("Make a choice")
+    print("9 - quit")
+    choice = input("Make a choice: ")
+    print(choice)
+    return choice
+
+interface()
