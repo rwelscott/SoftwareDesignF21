ae3b190362013f4dcf479c0162476559c6dc9ef2
Rachel Welscott <rachel.welscott@gmail.com>
2021-09-15 12:30:20 -0400

    Expanded testing range and fixed resulting code to pass tests

--- weight_entry.py
+++ weight_entry.py
@@ -8,7 +8,9 @@ def input_weight_entry():
 
 def parse_weight_input(weight_input):
     weight, units = weight_input.split(' ')
-    weight = int(weight)
+    weight = float(weight)
+    units = units.lower()
+    units = units.rstrip('s')
     if units == "lb":
         weight_kg = convert_lb_to_kg(weight)
     else:
