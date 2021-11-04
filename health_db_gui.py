cc5a9fe8c89a851ec5e4867c98f7bead57c14f68
Rachel Welscott <rachel.welscott@gmail.com>
2021-11-04 10:20:43 -0400

    Testing why the GUi doesn't pop up

--- health_db_gui.py
+++ health_db_gui.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+# from health_db_client import add_patient_to_server
 
-def create_output(name, id, blood_letter, rh_factor):
+def create_output(name, id, blood_letter, rh_factor, center):
     out_string = "Patient name: {}\n".format(name)
     out_string+= "Blood type: {}\n".format(blood_letter)
-    return out_string
+    out_string+= "Donation Center{}\n".format(center)
+    answer = add_patient_to_server(name, id, "{}{}".format(blood_letter,rh_factor))
+    return out_string, answer
 
 def design_window():
 
@@ -14,10 +17,12 @@ def design_window():
         id = id_data.get()
         blood_letter = blood_letter_data.get()
         rh_factor = rh_data.get()
+        center = donation_center_data.get()
         # Call external function to do the work that can be tested
-        answer = create_output(name, id, blood_letter, rh_factor)
+        answer = create_output(name, id, blood_letter, rh_factor, center)
         # Update GUI
         print(answer)
+        output_string.configure(text=answer)
 
     def cancel_button_cmd():
         root.destroy()
@@ -27,41 +32,55 @@ def design_window():
     root = tk.Tk()
     root.title("Health Database GUI")
     # root.geometry("1000x200")
-    # root.configure(bg='white')
+    # root.configure(bg='blue')
 
-    top_label = ttk.Label(root, text="Blood Donor Database")
-    top_label.grid(column=0, row=0)
+    frame = tk.Frame(root, bg="white")
+    frame.pack()
 
-    ttk.Label(root, text="Name").grid(column=0, row=1)
+    top_label = ttk.Label(frame, text="Blood Donor Database")
+    top_label.grid(column=0, row=0, columnspan=2, sticky='w')
+
+    ttk.Label(frame, text="Name").grid(column=0, row=1)
 
     name_data = tk.StringVar()
-    name_entry_box = ttk.Entry(root, width=50, textvariable=name_data)
-    name_entry_box.grid(column=1, row=1)
+    name_entry_box = ttk.Entry(frame, width=50, textvariable=name_data)
+    name_entry_box.grid(column=1, row=1, sticky='w', columnspan=2)
 
-    ttk.Label(root, text="ID").grid(column=0, row=2)
+    ttk.Label(frame, text="ID").grid(column=0, row=2)
 
     id_data = tk.StringVar()
-    id_entry_box = ttk.Entry(root, width=10, textvariable=id_data)
-    id_entry_box.grid(column=1, row=2)
+    id_entry_box = ttk.Entry(frame, width=10, textvariable=id_data)
+    id_entry_box.grid(column=1, row=2, sticky='w', padx=20, pady=20)
 
     blood_letter_data = tk.StringVar()
-    ttk.Radiobutton(root, text="A", variable=blood_letter_data,
+    ttk.Radiobutton(frame, text="A", variable=blood_letter_data,
                     value="A").grid(column=0, row=3, sticky=tk.W)
-    ttk.Radiobutton(root, text="B", variable=blood_letter_data,
+    ttk.Radiobutton(frame, text="B", variable=blood_letter_data,
                     value="B").grid(column=0, row=4, sticky=tk.W)
-    ttk.Radiobutton(root, text="AB", variable=blood_letter_data,
+    ttk.Radiobutton(frame, text="AB", variable=blood_letter_data,
                     value="AB").grid(column=0, row=5, sticky=tk.W)
-    ttk.Radiobutton(root, text="O", variable=blood_letter_data,
+    ttk.Radiobutton(frame, text="O", variable=blood_letter_data,
                     value="O").grid(column=0, row=6, sticky=tk.W)
 
     rh_data = tk.StringVar()
-    rh_checkbox = ttk.Checkbutton(root, text="Rh Positive", variable=rh_data, onvalue = "+", offvalue="-")
+    rh_data.set('-')
+    rh_checkbox = ttk.Checkbutton(frame, text="Rh Positive", variable=rh_data, onvalue = "+", offvalue="-")
     rh_checkbox.grid(column=1, row=4)
 
-    ok_button = ttk.Button(root, text="Ok", command=ok_button_cmd)
+    ttk.Label(root, text="Nearest Donation Center").grid(column=2, row=0)
+    donation_center_data = tk.StringVar()
+    combo_box = ttk.Combobox(root, textvariable=donation_center_data)
+    combo_box["values"] = ("Durham", "Raleigh", "Cary", "Apex")
+    combo_box.state(["readonly"])
+    combo_box.grid(column=3, row=1)
+
+    output_string = ttk.Label()
+    output_string.grid(column=1, row=10)
+
+    ok_button = ttk.Button(frame, text="Ok", command=ok_button_cmd)
     ok_button.grid(column=1, row=6)
 
-    cancel_button = ttk.Button(root, text="Cancel", command=cancel_button_cmd)
+    cancel_button = ttk.Button(frame, text="Cancel", command=cancel_button_cmd)
     cancel_button.grid(column=2, row=6)
 
     root.mainloop()
